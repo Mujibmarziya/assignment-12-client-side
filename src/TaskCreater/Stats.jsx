76fa82cc115eec57782c5a0ad26f536e080a73a2
Marziyamujib <s03552728@gmail.com>
2024-06-22 13:10:10 +0600

    pay button changed

--- src/TaskCreater/Stats.jsx
+++ src/TaskCreater/Stats.jsx
@@ -6,13 +6,7 @@ const Stats = ({coin}) => {
              <section className="p-6 my-6 dark:bg-gray-100 dark:text-gray-800">
 	<div className="container grid grid-cols-1 gap-6 mx-auto sm:grid-cols-2 xl:grid-cols-4">
 		
-		<div className="flex p-4 space-x-4 rounded-lg md:space-x-6 dark:bg-gray-50 dark:text-gray-800">
-			
-			<div className="flex flex-col justify-center align-middle">
-				<p className="text-3xl font-semibold leading-none">7500</p>
-				<p className="capitalize">My Total Tasks pore korbo</p>
-			</div>
-		</div>
+		
 		<div className="flex p-4 space-x-4 rounded-lg md:space-x-6 dark:bg-gray-50 dark:text-gray-800">
 			<div className="flex justify-center p-2 align-middle rounded-lg sm:p-4 dark:bg-violet-600">
 				<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="h-9 w-9 dark:text-gray-100">
